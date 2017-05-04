5a95c6f98b2ea89e61bdb71a0694a804b181db1f
Isabella Abatgis and Kristen McKee <isabella+kristen@>
2017-05-04 11:06:07 -0700

    Add front end logic to reject numbers outside of the range of 1 and 3999

---
+++ README.md
@@ -0,0 +1,62 @@
+# _Roman Numerals_
+
+#### _HTML CSS and JavaScript Webpage,_ _05.04.2017_
+
+#### By _**Isabella Abatgis and Kristen McKee**_
+
+## Description
+
+This a website that translates user input into roman numerals.
+
+## Specs
+
+- Detects if user input is non-numerical
+  - Input Example: dog
+  - Output Example: false
+
+- Convert numbers to letters
+  - Input: 5
+  - Output: V
+
+- Detects if number entered is between 1 and 3999
+  - Input Example: 4
+  - Output Example: IV
+
+- User input number is equivalent to sum of roman numeral values
+  - Input Example: 66
+  - Output Example: LXVI
+
+- If three of same roman numeral in a row is needed to satisfy sum, then use subtraction instead, subtracting left number from right number.
+  - Input Example: 90
+  - Output Example: XC
+
+- Roman Numeral values must only subtracted from roman numerals that share the same amount of digits or are 1 more digit
+  - Input Example: 99
+  - Output Example: XCIX
+
+## Setup/Installation Requirements
+
+* _Clone repository_
+* _Open in browser_
+
+## Support and contact details
+
+_Turn it off & turn it back on again._
+
+## Technologies Used
+
+_HTML_
+
+_CSS_
+
+_Bootstrap_
+
+_JavaScript_
+
+_JQuery_
+
+### License
+
+*MIT*
+
+Copyright (c) 2017 **_Isabella Abatgis_ & _Kristen McKee_**
---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+// Back End Logic //
+
+    var I = 1
+var II = 2
+var III = 3
+var IV = 4
+    var V = 5
+var VI = 6
+var VII = 7
+var VIII = 8
+var IX = 9
+    var X = 10
+var XX = 20
+var XXX = 30
+var XL = 40
+    var L = 50
+var LX = 60
+var LXX = 70
+var LXXX = 80
+var XC = 90
+    var C = 100
+var CC = 200
+var CCC = 300
+var CD = 400
+    var D = 500
+var DC = 600
+var DCC = 700
+var DCCC = 800
+var CM = 900
+    var M = 1000
+var MM = 2000
+var MMM = 3000
+
+var arrayMain = [I, V, X, L, C, D, M]
+var arraySubtract = [IV, IX, XL, XC, CD, CM]
+var arrayAll = [I, II, III, IV, V, VI, VII, VIII, IX, X, XX, XXX, XL, L, LX, LXX, LXXX, XC, C, CC, CCC, CD, D, DC, DCC, DCCC, CM, M, MM, MMM]
+var arrayOnes = [I, II, III, IV, V, VI, VII, VIII, IX]
+var arrayTens = [X, XX, XXX, XL, L, LX, LXX, LXXX, XC]
+var arrayHundres = [C, CC, CCC, CD, D, DC, DCC, DCCC, CM]
+var arrayThousands = [M, MM, MMM]
+var userInput = [];
+var output = [];
+
+for (var i = 0; i < arrayAll.length; i++) {
+  if (userInput.length === 1) {
+    var output = userInput.replace(userInput[0], "2");
+    alert(output);
+  }
+}
+
+// Front End Logic //
+
+$(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    userInput = parseInt($("#number").val());
+    if (userInput <= 0 || userInput > 3999) {
+      alert("Please enter a number between 1 and 3999");
+    }
+  })
+});
